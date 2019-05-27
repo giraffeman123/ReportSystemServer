40fa7e0f23bf7445cbef698e29c8b7e894811160
Mariano Garfel <48397351+Garfel@users.noreply.github.com>
2019-05-27 01:27:36 -0700

    commic changes java

--- ReportSystem.persistencia/src/test/java/Test.java
+++ ReportSystem.persistencia/src/test/java/Test.java
@@ -18,38 +18,4 @@ public class Test {
         
 System.out.println("Consulta"+ServiceLocator.getInstanceLiderDAO().findAll());
     }
-//        Lider l = new Lider();
-////        l.setCodigoEmpleado("AAAA");
-////        l.setNombre("Elliot");
-////        l.setArea("Fusebox"); 
-////        l.setFoto(null);
-////        l.setTurno("Matutino");
-////        ServiceLocator.getInstanceLiderDAO().save(l);
-//       // System.out.println("Consulta"+ServiceLocator.getInstanceLiderDAO().findAll());
-//    
-//      Lider r = new Lider();
-//           Empleado e = new Empleado();
-//          
-//          //LIDER
-//            r.setCodigoEmpleado("XXX");
-//            r.setLinea(2);
-//            r.setArea("Ingeniero");
-//            
-//          //EMPLEADO
-//            e.setNombre("Arellano");
-//            e.setPuesto("lider");
-//            e.setFoto(null);
-//            e.setTurno("tarder");
-//            e.setCodigoEmpleado("XXX");
-//           
-//            r.setEmpleado(e);
-//            ServiceLocator.getInstanceLiderDAO().update(r);
-//        
-//        
-//        
-//        
-//        
-//    }
-//    
-//    l.getCodigoEmpleado();
 }
